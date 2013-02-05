3ac02809047c1698618951d00af0edfc14003faa
David Lutz <del82@cornell.edu>
2013-02-05 18:23:33 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ezra::Application.config.secret_token = 'a369643b37472f5caf47a70ad8752cfb5eba068a6fb580096615aaecf97767fbb621e8d68b85bf8d00f69b8dd4f31a51a8a8e0d80f7c8c1cb20921620d8451f0'
