048a2378aeb54f85ad26d86369cd74bd67cfc3b0
David Lutz <del82@cornell.edu>
2013-02-09 11:03:04 -0500

    give up on doing anything intelligent with secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-#Ezra::Application.config.secret_token = 'you_should_replace_me_with_secret_token'
+Ezra::Application.config.secret_token = 'b5255e6e3ef238221523ffef2b82c333bb467ffea32be739ed0c75e218282c0e0747f9800a1a1687053e8dfaf3c4c4f9a061de82d71da79363619a5adb5a14f9'
