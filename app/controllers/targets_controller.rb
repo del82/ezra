8d8bc5365cf492a05f5be1dc0981304f632ee9a5
David Lutz <del82@cornell.edu>
2013-02-08 20:35:10 -0500

    more target view stuff?

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,15 +1,25 @@
 class TargetsController < ApplicationController
+  def index   # GET /targets          -> targets_path
+  end
 
-def new
-end
+  def show    # GET /targets/:id      -> target_path(target) 
+    @target = Target.find(params[:id])
+  end
 
-def index
-end
+  def new     # GET /targets/new      -> new_target_path
+  end
 
-def show
-end
+  def create  # POST /targets         -> targets_path
+  end
 
-def destroy
-end
+  def edit    # GET /targets/:id/edit -> edit_target_path(target)
+    @target = Target.find(params[:id])
+  end
+
+  def update  # PUT /targets/1        -> target_path(target)
+  end
+
+  def destroy # DELETE /targets/1     -> target_path(target)
+  end
 
 end
--- spec/requests/target_pages_spec.rb
+++ spec/requests/target_pages_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
-
+require 'requests/all_pages'
+  
 describe "Target pages" do
   subject { page }
+
+  describe "target index" do
+    before { visit targets_path }
+
+    describe "should have the right title" do
+      it_should_behave_like "all pages"
+      it { should have_selector('title', 
+                                text: full_title('All targets')) }
+    end
+  end
+
+  describe "individual target page" do
+    let(:target) { FactoryGirl.create(:target) }
+    before { visit target_path(target) }
+
+    describe "should have the right title" do
+      it_should_behave_like "all pages"
+      it { should have_selector('title', 
+                                text: full_title('Target | '+target.target_string)) }
+    end
+  end
+
+  describe "create target page" do
+    before { visit new_target_path }
+
+    describe "should have the right title" do
+      it_should_behave_like "all pages"
+      it { should have_selector('title', 
+                                text: full_title('New target')) }
+    end
+  end
+
+  describe "edit target page" do
+    let(:target) { FactoryGirl.create(:target) }
+    before { visit edit_target_path(target) }
+
+    describe "should have the right title" do
+      it_should_behave_like "all pages"
+      it { should have_selector('title', 
+                                text: full_title('Edit | '+target.target_string)) }
+    end
+  end
 end
