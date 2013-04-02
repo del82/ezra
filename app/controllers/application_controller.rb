b281a5d0a2e3dc77be54ab23b98bd0e0a449d23a
David Lutz <del82@cornell.edu>
2013-04-02 09:12:09 -0400

    static page links in header now generated from statics in db; issue #21

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
+  before_filter :make_statics_available
+
 protected
   def signed_in_user
     unless signed_in?
       store_location
       redirect_to signin_url, notice: "Please sign in."
     end
+  end
+
+  def make_statics_available
+    @statics = Static.all
   end
 end
