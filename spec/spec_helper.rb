9de70b126f6133e8b1d583cd4a192bfa209ba3f0
David Lutz <del82@cornell.edu>
2013-04-17 09:47:20 -0400

    add spec coverage generator
    set COVERAGE environment variable to true to create coverage report when
    running specs:
    
    $ COVERAGE=true rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,19 @@
 require 'rubygems'
 require 'spork'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/lib/'
+  add_filter '/vendor/'
+
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Views', 'app/views'
+end if ENV["COVERAGE"]
 
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
