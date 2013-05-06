6c4c3c4c55668856a4cdef8ad08c3a54e5d6c95e
David Lutz <del82@cornell.edu>
2013-05-06 17:41:34 -0400

    remove obsolete spec for 'password_digest'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,7 +38,6 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:username) }
   it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:admin) }
