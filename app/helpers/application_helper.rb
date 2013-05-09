4987583e782650af86c3e32212fa36745b48385e
David Lutz <del82@cornell.edu>
2013-05-09 19:40:21 -0400

    Fix #55 by building paths more intelligently

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,11 @@
 module ApplicationHelper
   def audio_path(path)
-    root = audio_root = ""
     if Ezra::Application.config.respond_to?(:relative_url_root)
       root = Ezra::Application.config.relative_url_root
     end
     if Ezra::Application.config.respond_to?(:audio_root)
       audio_root = Ezra::Application.config.audio_root
     end
-    return root + audio_root + path
+    return [root, audio_root, path].join('')
   end
 end
