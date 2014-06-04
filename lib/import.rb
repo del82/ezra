462e96c2ba90dc01b5f25973e79bc5a9cf3e1573
David Lutz <del82@cornell.edu>
2014-06-04 13:48:19 -0400

    make import module more complete

--- lib/import.rb
+++ lib/import.rb
@@ -44,11 +44,34 @@ module Import
     return true
   end
 
+  def self.make_small_audio_file(current_file, dest_dir, dest_fname)
+    full_dest = File.join(dest_dir, dest_fname)
+    if File.exist? full_dest
+      puts "small audio already exists: #{full_dest}"
+      return nil
+    end
+    if not File.exist? current_file
+      puts "file not found: #{current_file}"
+      return nil
+    end
+    dir = File.dirname full_dest
+    begin
+      FileUtils.mkdir_p dir
+    rescue SystemCallError
+      # dir already exists
+    end
+    command = "lame -a -q9 -b16 --quiet #{current_file} #{full_dest}"
+    puts "small audio command: #{command}"
+    return command
+  end
+
   def self.load_target(username, target_string, target_directory)  
     user = User.where(username: username).first
     target = Target.new(phrase: target_string)
     target.user_id = user.id
     target.save!
+    small_file = File.join("/home/ezra/import/", target_string.gsub(/\s+/, "")) + ".smallaudio"
+    smallfile = File.open(small_file, 'w')
     Dir.foreach target_directory do |f|
       if f =~ /param$/
         p_hash = self.load_param (File.join target_directory, f)
@@ -59,10 +82,15 @@ module Import
         audio_file_dest = p_hash["MP3"].gsub(/http:\/\//, "")
         audio_file_src  = File.join(target_directory, f.gsub(/param$/,"mp3"))
         audio_root = "/home/ezra/ezra/audio"
+	small_audio_root = "/home/ezra/ezra/public/smallaudio"
         full_dest = File.join(audio_root, audio_file_dest)
         puts "copy #{audio_file_src} to #{full_dest}" 
         self.process_audio_file(audio_file_src, audio_root, audio_file_dest)
-        h.audio_file = audio_file_dest
+	new_sa_command = self.make_small_audio_file(audio_file_src, small_audio_root, audio_file_dest)
+	if not new_sa_command.nil?
+          smallfile.write(new_sa_command + "\n")
+        end
+        h.audio_file = "audio/"+audio_file_dest
 
         ### transcript / timing
         h.transcript = "#{p_hash["LEFTCONTEXT"]} #{target_string} #{p_hash["RIGHTCONTEXT"]}"
@@ -72,6 +100,7 @@ module Import
         h.save!
       end
     end
+    smallfile.close
   end
 
 end # import module
